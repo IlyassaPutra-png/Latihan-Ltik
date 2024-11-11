f6c8efb1b953a5e1125b0491c69165b664902e7a
IlyassaPutra-png <ilyasputra6071@domain.com>
2024-11-11 23:00:04 +0700

    menambahkan codingan pada test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+'''
+Nama: Ilyassa Putra
+NIM: 2602741
+Kelas: 1-RPL
+'''
+
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 0:
+        return[0]
+    elif n == 2:
+        return[0,1]
+
+    deret_awal = [0,1]
+    for i in range(2, n):
+        deret_akhir = deret_awal [-1] + deret_awal [-2]
+        deret_awal.append(deret_akhir)
+    return deret_awal
+
+N = int(input("Masukan panjang deret fibonacci: "))
+
+print(f"Deret fibonacci sebanyak {N} angka: {fibonacci(N)}")
